98b7dcbe4e7d70bd96465e7e8bab6c009f27056d
Kevin <kevingtxz@gmail.com>
2022-03-16 15:16:07 -0300

    update again

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
-from .building_python_package.calc_kevin.calc_kevin import Calculator
+from project.calc_kevin.calc_kevin import Calculator
 
 sum = lambda a, b: Calculator.sum(a, b)
 sub = lambda a, b: Calculator.sub(a, b)
